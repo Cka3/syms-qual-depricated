68580b3d257d9ec0a11b3739961955dedd98b597
Sal Becker <physicsfu@gmail.com>
2017-06-03 16:36:28 -0400

    moving off electron for now

--- project.clj
+++ project.clj
@@ -1,72 +1,105 @@
-(defproject hello-electron "0.1.0-SNAPSHOT"
-  :source-paths ["src"]
-  :description "A hello world application for electron"
+(defproject syms-qual "0.1.0-SNAPSHOT"
+  :description "An Overwatch fan game written in Clojurescript"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :min-lein-version "2.7.1"
+
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [figwheel "0.5.8"]
-                 [reagent "0.6.0"]
-                 [ring/ring-core "1.5.0"]
-                 [binaryage/devtools "0.8.2"]
-                 [figwheel-sidecar "0.5.8"]
-                 [com.cemerick/piggieback "0.2.1"]]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [org.clojure/core.async  "0.3.442"
+                  :exclusions [org.clojure/tools.reader]]
+                 [reagent "0.6.0"]]
 
-  :plugins [[lein-cljsbuild "1.1.5"]
-            [lein-figwheel "0.5.8"]
-            [cider/cider-nrepl "0.12.0"]]
+  :plugins [[lein-figwheel "0.5.10"]
+            [lein-cljsbuild "1.1.5" :exclusions [[org.clojure/clojure]]]]
 
-  :clean-targets ^{:protect false} ["resources/main.js"
-                                    "resources/public/js/ui-core.js"
-                                    "resources/public/js/ui-core.js.map"
-                                    "resources/public/js/ui-out"]
-  :cljsbuild
-  {:builds
-   [{:source-paths ["electron_src"]
-     :id "electron-dev"
-     :compiler {:output-to "resources/main.js"
-                :output-dir "resources/public/js/electron-dev"
-                :optimizations :simple
-                :pretty-print true
-                :cache-analysis true}}
-    {:source-paths ["ui_src" "dev_src"]
-     :id "dev"
-     :compiler {:output-to "resources/public/js/ui-core.js"
-                :output-dir "resources/public/js/ui-out"
-                :source-map true
-                :asset-path "js/ui-out"
-                :optimizations :none
-                :cache-analysis true
-                :main "dev.core"
-                :preloads [devtools.preload]}}
-    {:source-paths ["electron_src"]
-     :id "electron-release"
-     :compiler {:output-to "resources/main.js"
-                :output-dir "resources/public/js/electron-release"
-                :optimizations :simple
-                :pretty-print true
-                :cache-analysis true}}
-    {:source-paths ["ui_src"]
-     :id "frontend-release"
-     :compiler {:output-to "resources/public/js/ui-core.js"
-                :output-dir "resources/public/js/ui-release-out"
-                :source-map "resources/public/js/ui-core.js.map"
-                :optimizations :simple
-                :cache-analysis true
-                :main "ui.core"}}]}
-  :figwheel {:http-server-root "public"
-             :css-dirs ["resources/public/css"]
-             :ring-handler tools.figwheel-middleware/app
-             :server-port 3449
-             :nrepl-port 7888}
+  :source-paths ["src"]
 
-  :profiles {:dev
-             {:dependencies [[binaryage/devtools "0.8.2"]
-                             [figwheel-sidecar "0.5.8"]
-                             [com.cemerick/piggieback "0.2.1"]]
-              ;; need to add dev source path here to get user.clj loaded
-              :source-paths ["ui_src" "dev_src"]
-              ;; for CIDER
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
 
-              :plugins [[cider/cider-nrepl "0.12.0"]]
-              :repl-options {; for nREPL dev you really need to limit output
-                             :init (set! *print-length* 50)
-                             :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
+                ;; the presence of a :figwheel configuration here
+                ;; will cause figwheel to inject the figwheel client
+                ;; into your build
+                :figwheel {:on-jsload "ui.core/on-js-reload"
+                           ;; :open-urls will pop open your application
+                           ;; in the default browser once Figwheel has
+                           ;; started and complied your application.
+                           ;; Comment this out once it no longer serves you.
+                           :open-urls ["http://localhost:3449/index.html"]}
+
+                :compiler {:main ui.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/game.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true
+                           ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
+                           ;; https://github.com/binaryage/cljs-devtools
+                           :preloads [devtools.preload]}}
+               ;; This next build is an compressed minified build for
+               ;; production. You can build this with:
+               ;; lein cljsbuild once min
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/game.js"
+                           :main ui.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
+             ;; :server-port 3449 ;; default
+             ;; :server-ip "127.0.0.1"
+
+             :css-dirs ["resources/public/css"] ;; watch and update CSS
+
+             ;; Start an nREPL server into the running figwheel process
+             :nrepl-port 7888
+
+             ;; Server Ring Handler (optional)
+             ;; if you want to embed a ring handler into the figwheel http-kit
+             ;; server, this is for simple ring servers, if this
+
+             ;; doesn't work for you just run your own server :) (see lein-ring)
+
+             ;; :ring-handler hello_world.server/handler
+
+             ;; To be able to open files in your editor from the heads up display
+             ;; you will need to put a script on your path.
+             ;; that script will have to take a file path and a line number
+             ;; ie. in  ~/bin/myfile-opener
+             ;; #! /bin/sh
+             ;; emacsclient -n +$2 $1
+             ;;
+             ;; :open-file-command "myfile-opener"
+
+             ;; if you are using emacsclient you can just use
+             ;; :open-file-command "emacsclient"
+
+             ;; if you want to disable the REPL
+             ;; :repl false
+
+             ;; to configure a different figwheel logfile path
+             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
+
+             ;; to pipe all the output to the repl
+             ;; :server-logfile false
+             }
+
+
+  ;; setting up nREPL for Figwheel and ClojureScript dev
+  ;; Please see:
+  ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.2"]
+                                  [figwheel-sidecar "0.5.10"]
+                                  [com.cemerick/piggieback "0.2.1"]]
+                   ;; need to add dev source path here to get user.clj loaded
+                   :source-paths ["src" "dev"]
+                   ;; for CIDER
+                   ;; :plugins [[cider/cider-nrepl "0.12.0"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                   ;; need to add the compliled assets to the :clean-targets
+                   :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                                     :target-path]}})
